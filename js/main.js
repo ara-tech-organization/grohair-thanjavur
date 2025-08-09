4cfcb7d0f86d27034b4f8d7a9af64c08f31c3d16
Samyuktha-ara <samyuktha.ara@gmail.com>
2025-08-09 16:20:37 +0530

    0908

--- js/main.js
+++ js/main.js
@@ -74,11 +74,16 @@
   });
   $(".img-carousel").owlCarousel({
     autoplay: true,
+    autoplayHoverPause: true, // stops when user hovers/taps
     smartSpeed: 1500,
     loop: true,
     margin: 20,
     dots: false,
-    nav: false,
+    nav: true, // enable arrows
+    navText: [
+      '<i class="fa fa-chevron-left"></i>',
+      '<i class="fa fa-chevron-right"></i>',
+    ],
     responsive: {
       0: {
         items: 1,
@@ -92,14 +97,60 @@
       992: {
         items: 4,
       },
-      1000: {
+      1200: {
         items: 5,
       },
     },
   });
+  $(".portfolio-carousel").owlCarousel({
+    autoplay: true,
+    autoplayHoverPause: true,
+    smartSpeed: 1200,
+    loop: true,
+    margin: 20,
+    dots: false,
+    nav: true,
+    navText: [
+      '<i class="fa fa-chevron-left"></i>',
+      '<i class="fa fa-chevron-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+
+  $(".treatment-carousel").owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 3000, // 3 seconds per slide
+    smartSpeed: 800,
+    margin: 20,
+    loop: true,
+    dots: false,
+    nav: true,
+    navText: [
+      "<i class='fa fa-chevron-left'></i>",
+      "<i class='fa fa-chevron-right'></i>",
+    ],
+    responsive: {
+      0: { items: 1 },
+      576: { items: 2 },
+      768: { items: 3 },
+      992: { items: 4 },
+    },
+  });
 
   // Pricing carousel
-  $(".pricing-carousel").owlCarousel({
+  var $carousel = $(".pricing-carousel");
+
+  $carousel.owlCarousel({
     autoplay: true,
     smartSpeed: 1500,
     margin: 30,
@@ -107,16 +158,20 @@
     dots: false,
     nav: false,
     responsive: {
-      0: {
-        items: 1,
-      },
-      576: {
-        items: 1,
-      },
-      768: {
-        items: 2,
-      },
+      0: { items: 1 },
+      576: { items: 1 },
+      768: { items: 2 },
     },
+  });
+
+  // Stop autoplay when video starts
+  $carousel.find("video").on("play", function () {
+    $carousel.trigger("stop.owl.autoplay");
+  });
+
+  // Resume autoplay when paused or ended
+  $carousel.find("video").on("pause ended", function () {
+    $carousel.trigger("play.owl.autoplay");
   });
 
   // Testimonialss carousel
@@ -129,8 +184,3 @@
     items: 1,
   });
 })(jQuery);
-
-
-
-
-
